c287778c3101c42509b46311fd62d4f0d940c644
Jonathan Norman <jnorman2123@gmail.com>
2019-11-14 03:06:04 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -24,7 +24,11 @@ class Application
       resp.write handle_search(search_term)
     elsif req.path.match(/add/)
       item_to_add = req.params["item"]
-      @@items.include?(item_to_add) ? @@items << item_to_add : resp.write "We don't have that item"
+      if @@items.include?(item_to_add)
+        @@items << item_to_add
+      else 
+        resp.write "We don't have that item"
+      end 
     else
       resp.write "Path Not Found"
     end
